2a3251f9435fa27e8a9c52631bac386570ef9567
Kim Seong-gwan <inertia0911@gmail.com>
2025-05-11 00:01:57 +0900

    first commit

---
+++ src/test/java/Oss/web_site/WebSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package Oss.web_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
